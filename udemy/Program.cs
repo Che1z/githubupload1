6f93369c9b5af62c358275b848bfba98b07c0399
Che1z <rain850218@gmail.com>
2023-12-26 10:55:16 +0800

    hw1-test-upload

--- udemy/Program.cs
+++ udemy/Program.cs
@@ -7,13 +7,30 @@
 
 public class Square {
     public int Side = 5;
-    public Square()
+    public Square(int side = 0)
     {
-
+        Side = side;
     }
 }
 
+// 習題2-1
 
+//double x, y, z;
+//Console.WriteLine("請輸入x");
+//x = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("請輸入y");
+//y = Convert.ToDouble(Console.ReadLine());
+
+//if (x > y)
+//{
+//    z = x;
+//    Console.WriteLine($"結果為：{z}");
+//}
+//else
+//{
+//    z = y;
+//    Console.WriteLine($"結果為：{z}");
+//}
 
 
 
